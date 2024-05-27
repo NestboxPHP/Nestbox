38dc19eaa3889e47ca27b87308e5ede4bf76e23d
supergnaw <loren.supernaw@gmail.com>
2024-05-27 05:29:44 -0500

    Update QueryErrorException.php
    - moved pdo and statement error parsers outside of exception

--- src/Exception/QueryErrorException.php
+++ src/Exception/QueryErrorException.php
@@ -6,10 +6,8 @@ namespace NestboxPHP\Nestbox\Exception;
 
 class QueryErrorException extends NestboxException
 {
-    function __construct(string $message = "", int $code = 0, $previous = null, array $errorInfo = [])
+    function __construct(string $message = "", int $code = 0, $previous = null)
     {
-        $message = (!$message and $errorInfo) ? "MySQL error $errorInfo[1]: $errorInfo[2] ($errorInfo[0])" : $message;
-
         parent::__construct($message, $code, $previous);
     }
 }
