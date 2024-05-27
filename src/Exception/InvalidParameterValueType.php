b501509de9c3b3d52cdaea6c18908e016b9de81f
supergnaw <loren.supernaw@gmail.com>
2024-05-27 05:27:13 -0500

    Update InvalidParameterValueType.php
    - Added value parser to stringify values that are invalid.

--- src/Exception/InvalidParameterValueType.php
+++ src/Exception/InvalidParameterValueType.php
@@ -6,8 +6,14 @@ namespace NestboxPHP\Nestbox\Exception;
 
 class InvalidParameterValueType extends NestboxException
 {
-    function __construct(string $message = "", int $code = 0, $previous = null)
+    function __construct(string $message = "", int $code = 0, \Exception $previous = null, string $variable = "", mixed $value = null)
     {
-        parent::__construct($message, $code, $previous);
+        $type = gettype($value);
+        $value = match ($type) {
+            "boolean", "integer", "double", "string" => $value,
+            "NULL" => "NULL",
+            default => serialize($value)
+        };
+        parent::__construct(trim("$message $variable ($type): $value"), $code, $previous);
     }
 }
